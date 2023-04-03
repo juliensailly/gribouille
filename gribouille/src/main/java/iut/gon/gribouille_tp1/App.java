22789cfe69fbceaad0ce318d3105dc461b1b521c
Julien Sailly <juliensailly76@gmail.com>
2023-04-03 21:20:53 +0200

    Merge avec gribouille_tp2

--- gribouille/src/main/java/iut/gon/gribouille_tp1/App.java
+++ gribouille/src/main/java/iut/gon/gribouille_tp1/App.java
@@ -1,38 +1,58 @@
 package iut.gon.gribouille_tp1;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.event.EventType;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 /**
  * JavaFX App
  */
 public class App extends Application {
 
-    private static Scene scene;
+	private static Scene scene;
+	private double prevX;
+	private double prevY;
 
-    @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("CadreGribouille"), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
+	@Override
+	public void start(Stage stage) throws IOException {
+		scene = new Scene(loadFXML("CadreGribouille"), 640, 480);
+		stage.setScene(scene);
+		stage.setTitle("Gribouille");
+		stage.getIcons().add(new Image("file:src\\main\\resources\\iut\\gon\\gribouille_tp1\\icon.jpg"));
+		stage.show();
+		Dialogues.prepareFermeture(stage);
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
+		Canvas dessin = (Canvas) scene.lookup("Canvas");
+		dessin.addEventHandler(MouseEvent.MOUSE_PRESSED, (click) -> {
+			this.prevX = click.getX();
+			this.prevY = click.getY();
+		});
+		dessin.addEventHandler(MouseEvent.MOUSE_DRAGGED, (appui) -> {
+			dessin.getGraphicsContext2D().strokeLine(prevX, prevY, appui.getX(), appui.getY());
+			this.prevX = appui.getX();
+			this.prevY = appui.getY();
+		});
+	}
 
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
+	static void setRoot(String fxml) throws IOException {
+		scene.setRoot(loadFXML(fxml));
+	}
 
-    public static void main(String[] args) {
-        launch();
-    }
+	private static Parent loadFXML(String fxml) throws IOException {
+		FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+		return fxmlLoader.load();
+	}
+
+	public static void main(String[] args) {
+		launch();
+	}
 
 }
