d5d22ce111610bbcd8734b1629abb232b66a8ec0
Julien Sailly <juliensailly76@gmail.com>
2023-04-09 19:33:19 +0200

    Ajout du package modele

--- gribouille/src/main/java/iut/gon/gribouille/App.java
+++ gribouille/src/main/java/iut/gon/gribouille/App.java
@@ -3,13 +3,12 @@ package iut.gon.gribouille;
 
 import java.io.IOException;
 
+import iut.gon.modele.Dessin;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
 import javafx.scene.image.Image;
-import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 /**
