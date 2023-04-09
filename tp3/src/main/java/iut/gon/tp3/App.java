194c893f5dc2c627f815cc8f9115fd41f09b7e6c
Julien Sailly <juliensailly76@gmail.com>
2023-04-09 17:10:15 +0200

    Ajout de l'EventHandler KEY_PRESSED pour le contrôleur clavier

--- tp3/src/main/java/iut/gon/tp3/App.java
+++ tp3/src/main/java/iut/gon/tp3/App.java
@@ -6,6 +6,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 /**
@@ -19,6 +20,38 @@ public class App extends Application {
 	public void start(Stage stage) throws IOException {
 		GrilleModel gm = new GrilleModel();
 		GrilleController gc = new GrilleController(gm);
+		stage.addEventHandler(KeyEvent.KEY_PRESSED, event -> {
+			String textLabel = "Touche";
+			switch (event.getText()) {
+			case "1":
+				gm.setCase(textLabel, 2, 0);
+				break;
+			case "2":
+				gm.setCase(textLabel, 2, 1);
+				break;
+			case "3":
+				gm.setCase(textLabel, 2, 2);
+				break;
+			case "4":
+				gm.setCase(textLabel, 1, 0);
+				break;
+			case "5":
+				gm.setCase(textLabel, 1, 1);
+				break;
+			case "6":
+				gm.setCase(textLabel, 1, 2);
+				break;
+			case "7":
+				gm.setCase(textLabel, 0, 0);
+				break;
+			case "8":
+				gm.setCase(textLabel, 0, 1);
+				break;
+			case "9":
+				gm.setCase(textLabel, 0, 2);
+				break;
+			}
+		});
 		scene = new Scene(loadFXML("scene", gc), 640, 480);
 		stage.setScene(scene);
 		stage.show();
