a23e2e9c04fcfad6430c64b44745649177212cbb
Julien Sailly <juliensailly76@gmail.com>
2023-05-05 21:45:59 +0200

    Modification de DessinController

--- gribouille/src/main/java/controleurs/Controleur.java
+++ gribouille/src/main/java/controleurs/Controleur.java
@@ -3,13 +3,16 @@ package controleurs;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import iut.gon.gribouille.GribouilleController;
 import iut.gon.modele.Dessin;
 import iut.gon.modele.Figure;
+import javafx.beans.binding.Bindings;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.fxml.FXML;
 import javafx.scene.paint.Color;
+import javafx.util.converter.NumberStringConverter;
 
 public class Controleur {
 	public final Dessin dessin = new Dessin();
@@ -22,10 +25,10 @@ public class Controleur {
 	public SimpleObjectProperty<Color> couleur;
 	public SimpleIntegerProperty epaisseur;
 
-	private @FXML CouleursController couleursController;
-	private @FXML DessinController dessinController;
-	private @FXML MenusController menusController;
-	private @FXML StatutController statutController;
+	public @FXML CouleursController couleursController;
+	public @FXML DessinController dessinController;
+	public @FXML MenusController menusController;
+	public @FXML StatutController statutController;
 	
 	public void initialize(URL url, ResourceBundle ressourceBundle) {
 		prevX = new SimpleDoubleProperty();
@@ -37,5 +40,16 @@ public class Controleur {
 		dessinController.setControleur(this);
 		menusController.setControleur(this);
 		statutController.setControleur(this);
+		
+		dessinController.centralCanva.widthProperty().bind(dessinController.central_pane.widthProperty());
+		dessinController.centralCanva.heightProperty().bind(dessinController.central_pane.heightProperty());
+		dessinController.centralCanva.widthProperty().addListener(obs -> dessinController.reDraw());
+		dessinController.centralCanva.heightProperty().addListener(obs -> dessinController.reDraw());
+		Bindings.bindBidirectional(statutController.XlabelValue.textProperty(), prevX, new NumberStringConverter());
+		Bindings.bindBidirectional(statutController.YlabelValue.textProperty(), prevY, new NumberStringConverter());
+		
+		dessinController.index = -1;
+		prevX.set(0);
+		prevY.set(0);
 	}
 }
--- gribouille/src/main/java/controleurs/DessinController.java
+++ gribouille/src/main/java/controleurs/DessinController.java
@@ -1,7 +1,10 @@
 package controleurs;
 
+import iut.gon.modele.Figure;
+import iut.gon.modele.Trace;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 
 public class DessinController {
@@ -11,8 +14,73 @@ public class DessinController {
 	public Pane central_pane;
 	@FXML
 	Controleur controleur;
+	
+	public int index;
 
 	public void setControleur(Controleur c) {
 		this.controleur = c;
+	}
+	
+	/**
+	 * Fonction permettant de redessinner le canvas lors du redimensionnement de la fenêtre et donc du canvas.
+	 */
+	public void reDraw() {
+		// Efface le canva afin d'éviter de superposer les traits.
+		centralCanva.getGraphicsContext2D().clearRect(0, 0, centralCanva.getWidth(), centralCanva.getHeight()); 
+
+		for (Figure trace : controleur.dessin.getFigures()) {
+			controleur.prevX.set(trace.getPoints().get(0).getX());
+			controleur.prevY.set(trace.getPoints().get(0).getY());
+
+			for (int i = 1; i < trace.getPoints().size(); i++) {
+				centralCanva.getGraphicsContext2D().strokeLine(controleur.prevX.get(), controleur.prevY.get(),
+						trace.getPoints().get(i).getX(), trace.getPoints().get(i).getY());
+				controleur.prevX.set(trace.getPoints().get(i).getX());
+				controleur.prevY.set(trace.getPoints().get(i).getY());
+			}
+		}
+	}
+	
+	/**
+	 * Fonction appelée lors du click du canvas.
+	 * Elle permet de commencer un tracé.
+	 * 
+	 * @param evt Utilisé pour récupérer les coordonnées de la souris.
+	 */
+	public void onMousePressed(@SuppressWarnings("exports") MouseEvent evt) {
+		controleur.prevX.set(evt.getX());
+		controleur.prevY.set(evt.getY());
+		index++;
+		controleur.dessin.getFigures().add(new Trace(1, "noir", evt.getX(), evt.getY()));
+	}
+	
+	/**
+	 * Fonction appelée lors d'un click-déplacement de la souris sur le canvas.
+	 * Elle permet de dessiner des liaisons entre les points cliqués.
+	 * 
+	 * @param evt Utilisé pour récupérer les coordonnées de la souris.
+	 */
+	public void onMouseDragged(@SuppressWarnings("exports") MouseEvent evt) {
+		centralCanva.getGraphicsContext2D().strokeLine(controleur.prevX.get(), controleur.prevY.get(), evt.getX(), evt.getY());
+		controleur.prevX.set(evt.getX());
+		controleur.prevY.set(evt.getY());
+		controleur.dessin.getFigures().get(index).addPoint(evt.getX(), evt.getY());
+	}
+
+	/**
+	 * Fonction permettant d'afficher les coordonnées de la souris dans les Labels situés sous le canvas.
+	 * 
+	 * @param evt Utilisé pour récupérer les coordonnées de la souris.
+	 */
+	public void onMouseMoved(@SuppressWarnings("exports") MouseEvent evt) {
+		// On teste la longueur des coordonnées pour éviter les problèmes d'affichage
+		controleur.statutController.XlabelValue.textProperty()
+				.set(Double.toString(evt.getX()).length() > 5 ? 
+						Double.toString(evt.getX()).substring(0, 5)
+						: Double.toString(evt.getX()));
+		controleur.statutController.YlabelValue.textProperty()
+		.set(Double.toString(evt.getY()).length() > 5 ? 
+				Double.toString(evt.getY()).substring(0, 5)
+				: Double.toString(evt.getY()));
 	}
 }
